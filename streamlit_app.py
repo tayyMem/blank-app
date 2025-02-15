7e82a2f992c59c6ada413eb491f715849348d317
tayyMem <invaliduzer@gmail.com>
2025-02-15 20:26:33 +0000

    update title - by adding name properly

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My Streamlit app - Proj1")
+st.title("🎈 My Streamlit app - Project 1")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
